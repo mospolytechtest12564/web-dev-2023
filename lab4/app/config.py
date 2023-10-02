b351350fcbd5443a399476cb3779e3be5f3afa6a
Maksim Vasilchuk <max.vas100803@mail.ru>
2023-10-02 05:45:51 +0300

    lab4

--- lab4/app/config.py
+++ lab4/app/config.py
@@ -1,6 +1,6 @@
 SECRET_KEY = '9695cce7d2daa4d9c7f952d1cca92b6c9183e5907262017d987e60de07801749'
 
-MYSQL_USER = 'std_1906_app_crud'
-MYSQL_PASSWORD = 'qwertyuiop'
+MYSQL_USER = 'std_1859_lab4'
+MYSQL_PASSWORD = 'maks2003'
 MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_DATABASE = 'std_1906_app_crud'
+MYSQL_DATABASE = 'std_1859_lab4'
